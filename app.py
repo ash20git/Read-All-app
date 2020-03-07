667e505a0f31ca717ee26e1da0c36c16ba569ac1
ash20git <ashuguitar@hotmail.com>
2020-03-07 22:37:43 +0530

    HELLO

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect
+import test
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_username():
+          
+    return render_template('first_page.html')
+
+@app.route('/',methods=['POST'])
+def index():    
+    if request.method == 'POST':
+        user = request.form['username']
+        if user == '':
+            return render_template('home.html',user='user')
+
+    return render_template('home.html',user=user)
+
+@app.route('/news')
+def get_news(): 
+
+    item = test.cat_news()  
+    return render_template('news.html',item=item, cat='news')
+    
+@app.route('/sports/<cat>')
+def get_sports(cat):
+
+    news_sports_x = test.cat_sports(cat)
+    return render_template('sports.html',news_sports_x=news_sports_x)
+
+@app.route('/img')
+def img_top():
+
+    list_images = test.img_coll()
+    return render_template('font.html',list_images=list_images)
+         
+if __name__ == "__main__":  
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,100 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+sports_category_list = ['cricket','tennis','football']
+
+def cat_news():
+    cnt = 0
+    news_li = []
+    url = 'https://www.tribuneindia.com/'
+
+    page = requests.get(url)
+
+    s = BeautifulSoup(page.text,'html.parser')
+
+    item = s.find_all('a', class_='card-top-align')
+
+    for i in item:
+        cnt+=1
+        if cnt>12:
+            break
+        i= i.text.strip()
+        news_li.append(i)
+
+    
+    # print(item)
+    return news_li
+
+
+def cat_sports(x):
+
+    num=1
+    sports_x = []
+
+    if x in sports_category_list:
+        url = 'https://indianexpress.com/section/sports/' + str(x) + '/'  
+    else:
+        return "ERROR IN SCRAPING"
+
+
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.text,'html.parser')
+
+    base = soup.find_all('div',class_='articles')
+
+    for b in base:
+        num+=1
+        if num>15:
+            break
+        item = b.find('h2',class_='title')
+        item = item.text
+        sports_x.append(item)
+
+    return sports_x
+
+
+def top_story_home():
+
+    url = 'https://www.hindustantimes.com/top-news/'
+
+    page = requests.get(url)
+
+    img_found = BeautifulSoup(page.text,'html.parser')
+    
+    img_found = img_found.find('img', class_='img-responsive')
+
+    img_text = img_found['title']
+
+    img_href = img_found['data-src']
+
+    return img_text, img_href
+
+def img_coll():
+    cnt=0
+    img_href = []
+    img_text = []
+    url = 'https://www.hindustantimes.com/top-news/'
+
+    page = requests.get(url)
+
+    obj = BeautifulSoup(page.text,'html.parser')
+
+    img = obj.find_all('div', class_='relative-icons')
+
+    for i in img:
+        cnt+=1
+        if cnt>5:
+            break
+
+        i=i.find('img')
+        img_href.append(i['src'])
+        img_text.append(i['title'])
+
+
+    x = list(zip(img_href,img_text))
+
+    return x
+
+
